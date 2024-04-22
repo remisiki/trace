95a6d26ebfe34230d8e7e32eb329efd93c048e1d
Yiwen Yang <akiyiwen@gmail.com>
2024-04-21 20:49:17 -0400

    fix: dev api endpoint

--- client/src/common/Api.ts
+++ client/src/common/Api.ts
@@ -11,7 +11,7 @@ namespace Api {
    */
   export const endPoint =
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-      ? "http://localhost:8000"
+      ? "http://localhost:8000/api"
       : `${process.env.PUBLIC_URL}/api`;
 
   /**
